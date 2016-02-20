4787e0a524586151896b1bdd31aa9e082ed929ba
AgafontsevaElena <ea.malyshkina@gmail.com>
2016-02-20 14:18:16 +0300

    По замечаниям от 20.02.2016

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var button = document.querySelector(".search-hotels-title");
+var form = document.querySelector(".search-hotels form");
+	button.addEventListener("click", function() { 
+        form.classList.toggle("show");
+          });
