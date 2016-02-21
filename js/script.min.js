0d1a0a1d83cc0e781626c520de9177f20ca96c6f
AgafontsevaElena <ea.malyshkina@gmail.com>
2016-02-21 20:01:21 +0300

    Замечания
    Разное Б15 (единообразие css html), тестирование Б17 п.1 (паддинги),
    переименованы style.min.css script.min.js

---
+++ js/script.min.js
@@ -0,0 +1,1 @@
+var button=document.querySelector(".search-hotels-title"),form=document.querySelector(".search-hotels form");button.addEventListener("click",function(){form.classList.toggle("show")});
