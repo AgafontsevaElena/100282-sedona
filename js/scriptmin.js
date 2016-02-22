ea174ba5990d62bd0774d437da28ef75fe0864d6
AgafontsevaElena <ea.malyshkina@gmail.com>
2016-02-22 21:04:44 +0300

    Минификация, удалены  неиспользуемые файлы

--- js/scriptmin.js
+++
@@ -1,1 +0,0 @@
-var button=document.querySelector(".search-hotels-title"),form=document.querySelector(".search-hotels form");button.addEventListener("click",function(){form.classList.toggle("show")});
